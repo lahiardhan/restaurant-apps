75125a97ceb30ce2b7f11993c3039f8303a9e50c
Muhammad Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-11-06 17:46:18 +0700

    like button

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -2,7 +2,10 @@ const CONFIG = {
   KEY: 'YOUR_API_KEY',
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/large/',
-  CACHE_NAME: new Date().toISOString(),
+  CACHE_NAME: 'DaharRestaurant-V1',
+  DATABASE_NAME: 'dahar-restaurants-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'restaurants',
 };
 
 export default CONFIG;
--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,11 +1,13 @@
 import RestaurantSource from '../../data/restaurant-sources';
 import UrlParser from '../../routes/url-parser';
+import LikeButtonInitiator from '../../utils/like-button-initiator';
 import { createRestaurantDetailTemplate } from '../templates/template-creator';
 
 const Detail = {
   async  render() {
     return `
       <div id="restaurant" class="restaurant" ></div>
+      <div id="likeButtonContainer"></div>
     `;
   },
 
@@ -13,7 +15,19 @@ const Detail = {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
     const restaurant = await RestaurantSource.restaurantDetail(url.id);
     const restaurantContainer = document.querySelector('#restaurant');
+
     restaurantContainer.innerHTML = createRestaurantDetailTemplate(restaurant.restaurant);
+
+    LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: restaurant.id,
+        name: restaurant.name,
+        rating: restaurant.rating,
+        city: restaurant.city,
+        thumbnail: restaurant.pictureId,
+      },
+    });
   },
 };
 
