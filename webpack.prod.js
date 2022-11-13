9ed47bac790e3d561111ae831c6b32f9ad51be9d
Muhammad Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-11-13 13:21:34 +0700

    optimized performance

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,7 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
@@ -52,6 +52,6 @@ module.exports = merge(common, {
       swSrc: path.resolve(__dirname, 'src/scripts/sw.js'),
       swDest: './sw.bundle.js',
     }),
-    new BundleAnalyzerPlugin(),
+    // new BundleAnalyzerPlugin(),
   ],
 });
